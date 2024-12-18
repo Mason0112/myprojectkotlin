e401c0e3cda4b3e4a9d2e48e7bb4d7ac4e5ad58c
DESKTOP-NELQMJ0\Mason <willy55406@gmail.com>
2024-12-18 17:18:33 +0800

    12/18

--- src/main/kotlin/ticket/Ticket.kt
+++ src/main/kotlin/ticket/Ticket.kt
@@ -1,5 +1,7 @@
 package org.example.ticket
 
+import java.util.*
+
 
 data  class Ticket(val origin:Int, val destination:Int, val price:Int){
 
@@ -7,7 +9,25 @@ data  class Ticket(val origin:Int, val destination:Int, val price:Int){
 
 fun main() {
     var ticket = Ticket(20, 51, 420)
+    val s = "dadasdasdad"
+    println(s.validate())
+    val s1 = "aaa"
+    println(s1.validate())
+    println((1..20).random())
 
+
+
+}
+
+//擴充方法
+fun String.validate():Boolean{
+    return this.length >= 6
+}
+
+//擴充Range ex.(1..10)
+fun IntRange.random() : Int{
+    val r = Random().nextInt(endInclusive - start) + start
+    return r
 }
 
 
